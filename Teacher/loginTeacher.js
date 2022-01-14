58f9178d6d5ee8d3667a1038a86e318901cc6034
saeid <saeid.noormohammad@gmail.com>
2022-01-14 15:50:30 +0300

    changes

--- Teacher/loginTeacher.js
+++ Teacher/loginTeacher.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const { ApolloError, AuthenticationError } = require("apollo-server");
 const { jwtGenarate } = require("../middleware/jwt_login");
+const jwt = require("jsonwebtoken");
 
 const loginTeacher_f = async (email, password, pool, res) => {
   const exist = await pool.query("SELECT * FROM teacher WHERE email=$1", [
@@ -14,53 +15,80 @@ const loginTeacher_f = async (email, password, pool, res) => {
   if (confrimPassword == false) {
     throw new AuthenticationError("Email Or Password Is Wrong!");
   }
-  const { accessToken, refreshToken } = await jwtGenarate(
-    user.email,
-    user.name,
-    user.teacher_id,
-    user.role
-  );
-  await pool.query("UPDATE teacher SET refresh_token=$1 WHERE teacher_id=$2", [
-    refreshToken,
-    user.teacher_id,
-  ]);
-  res.cookie("access_token", accessToken, {
-    maxAge: 1000 * 60 * 60,
-    // sameSite: "none",
-    // domain: ".math-p.vercel.app",
-    // process.env.NODE_ENV === "production" ? "math-p.vercel.app" : "localhost",
-    // sameSite: "Stric",
-    // path: "/",
-    secure: process.env.NODE_ENV === "production",
-    // secure: false,
-    // httpOnly: true,
+  if (user.refresh_token) {
+    try {
+      await jwt.verify(user.refresh_token, process.env.REFRESH_TOKEN);
+      const access = await sign(
+        {
+          email: user.email,
+          name: user.name,
+          id: user.teacher_id,
+          role: user.role,
+        },
+        process.env.ACCESS_TOKEN,
+        { expiresIn: "1m" }
+      );
+      res.cookie("access_token", access, {
+        maxAge: 1000 * 60 * 60,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "",
+      });
+      res.cookie("refresh_token", user.refresh_token, {
+        maxAge: 1000 * 60 * 60 * 24,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "",
+      });
+      return { accessToken, refreshToken };
+    } catch (error) {
+      const { accessToken, refreshToken } = await jwtGenarate(
+        user.email,
+        user.name,
+        user.teacher_id,
+        user.role
+      );
 
-    // sameSite: "none",
-    // domain: "localhost",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "",
-    // secure: process.env.NODE_ENV ? true : false,
-  });
-  res.cookie("refresh_token", refreshToken, {
-    maxAge: 1000 * 60 * 60 * 24,
-    // domain: "localhost",
-    // domain: ".math-p.vercel.app",
-    // process.env.NODE_ENV === "production" ? "math-p.vercel.app" : "localhost",
-    secure: process.env.NODE_ENV === "production",
-    // sameSite: "none",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "",
-    // path: "/",
-    // secure: process.env.NODE_ENV ? true : false,
-    // httpOnly: true,
-    // domain: "math-p.vercel.app",
-    // secure: process.env.NODE_ENV === "production",
+      await pool.query(
+        "UPDATE teacher SET refresh_token=$1 WHERE teacher_id=$2",
+        [refreshToken, user.teacher_id]
+      );
+      res.cookie("access_token", accessToken, {
+        maxAge: 1000 * 60 * 60,
+        // sameSite: "none",
+        // domain: ".math-p.vercel.app",
+        // process.env.NODE_ENV === "production" ? "math-p.vercel.app" : "localhost",
+        // sameSite: "Stric",
+        // path: "/",
+        secure: process.env.NODE_ENV === "production",
+        // secure: false,
+        // httpOnly: true,
 
-    // secure: true,
-    // sameSite: "lax",
+        // sameSite: "none",
+        // domain: "localhost",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "",
+        // secure: process.env.NODE_ENV ? true : false,
+      });
+      res.cookie("refresh_token", refreshToken, {
+        maxAge: 1000 * 60 * 60 * 24,
+        // domain: "localhost",
+        // domain: ".math-p.vercel.app",
+        // process.env.NODE_ENV === "production" ? "math-p.vercel.app" : "localhost",
+        secure: process.env.NODE_ENV === "production",
+        // sameSite: "none",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "",
+        // path: "/",
+        // secure: process.env.NODE_ENV ? true : false,
+        // httpOnly: true,
+        // domain: "math-p.vercel.app",
+        // secure: process.env.NODE_ENV === "production",
 
-    // secure: process.env.NODE_ENV ? true : false,
-  });
+        // secure: true,
+        // sameSite: "lax",
 
-  return { accessToken, refreshToken };
+        // secure: process.env.NODE_ENV ? true : false,
+      });
+      return { accessToken, refreshToken };
+    }
+  }
 };
 
 module.exports = { loginTeacher_f };
